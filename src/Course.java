d2367491f64bd4e27bdaade26e742742b110110a
Rosalea Anderson <rjanderson@mymail.mines.edu>
2018-11-15 12:03:02 -0700

    Implemented file reader

--- src/Course.java
+++ src/Course.java
@@ -4,15 +4,15 @@ public class Course {
 	private ArrayList<Course> prereqs;
 	public String courseName;
 	public String courseID;
-	private int creditHours;
+	private double creditHours;
 	private String semesterOffered;
 	
-	public Course(String name, String ID, int credit, String semester) {
+	public Course(String name, String ID, double credit) {
 		this.prereqs = new ArrayList<Course>();
 		this.courseName = name;
 		this.courseID = ID;
 		this.creditHours = credit;
-		this.semesterOffered = semester;
+//		this.semesterOffered = semester;
 	}
 	
 	public void setPrereqs(ArrayList<Course> p) {
--- src/SchedulePlanner.java
+++ src/SchedulePlanner.java
@@ -1,8 +1,39 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SchedulePlanner {
+	private ArrayList<Course> classesForMajor = new ArrayList<Course>();
 	// read in a list of classes required courses
+	public void loadCourses() {
+		String txtSplitBy = ", ";
+		String courseName = "";
+		String courseID = "";
+		double creditHours = 0.0;
+		
+			FileReader reader;
+			try {
+				reader = new FileReader("BiologicalEngineeringCourses.txt");
+				Scanner in = new Scanner (reader);
+				
+				while(in.hasNextLine()) {
+					String line = in.nextLine();
+					String[] currentLine = line.split(txtSplitBy);
+					
+					courseID = currentLine[0];
+					courseName = currentLine[1];
+					creditHours = Double.parseDouble(currentLine[3]);
+					
+					classesForMajor.add(new Course(courseName, courseID, creditHours));
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	
 	// compare to a list of courses that have already been taken
 	// generate a schedule based on some constraints, number of desired semesters etc.
 	// must know what the prerequisites to each class are
-	// 
-}
+	}
+	
